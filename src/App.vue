4076f8cb3b5a9828a2a05eed0953c5d86d5530f0
amy10260903 <amy10260903@gmail.com>
2022-01-06 13:57:12 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="todo-main">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+  /* Base */
+  .container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
