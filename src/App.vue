6a0d763e61ad76ed006cfa4ad62d715073010e76
amy10260903 <amy10260903@gmail.com>
2022-01-06 14:19:08 +0800

    add layouts

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div class="container">
       <div class="todo-main">
-
+        <div class="todo-header">
+          <input type="text" placeholder="Enter something..."/>
+        </div>
       </div>
     </div>
   </div>
@@ -22,5 +24,18 @@ export default {
   .container {
     width: 100%;
     height: 100%;
+  }
+  .todo-main {
+    margin: 50px 220px;
+    text-align: center;
+  }
+
+  /* Header */
+  .todo-header {
+    width: 100%;
+  }
+  .todo-header input {
+    width: 100%;
+    height: 26px;
   }
 </style>
